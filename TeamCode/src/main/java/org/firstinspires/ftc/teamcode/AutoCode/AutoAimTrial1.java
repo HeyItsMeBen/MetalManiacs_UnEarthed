c91a10437f385dd3f2d24d397ea457b77f8fb337
PhantLatte <90583294+PhantLatte@users.noreply.github.com>
2025-10-03 16:46:17 -0700

    Update AutoAimTrial1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/AutoAimTrial1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCode/AutoAimTrial1.java
@@ -127,10 +127,10 @@ public class AutoAimTrial1 extends LinearOpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must match the names assigned during the robot configuration.
         // step (using the FTC Robot Controller app on the phone).
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
