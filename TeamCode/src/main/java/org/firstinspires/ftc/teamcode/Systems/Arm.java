9b4a62db3c8d68d27184c1fffacdee460c3b3583
FriendOfTheFire <andrewscientist09@gmail.com>
2025-10-16 21:42:39 -0700

    Update PID values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Arm.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm {
     private PIDController controller;
-    public static double p = 0.01, i = 0.05, d = 0.001;
+    public static double p = 0.001, i = 0, d = 0;
     public static double f = 0;
     private final double ticks_in_degree = 1120 / 360;
     private DcMotorEx arm_motor;
