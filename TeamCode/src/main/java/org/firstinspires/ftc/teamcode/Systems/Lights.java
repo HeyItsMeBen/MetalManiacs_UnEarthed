4d2c92814ced888fe91ec15b7837b9222b3458ed
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2025-10-29 19:08:21 -0700

    Light code added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Lights.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Systems;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Lights {
+    RevBlinkinLedDriver blinkinLedDriver;
+
+    public Lights(HardwareMap hardwareMap){ //Run this in Int to map the class items
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "LED");
+    }
+
+    public void Light_Red(){
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+    }
+
+    public void Light_Green(){
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+        blinkinLedDriver.close();
+    }
+
+    public void Light_Blue(){
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+    }
+
+    public void Light_Off(){
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+    }
+}
