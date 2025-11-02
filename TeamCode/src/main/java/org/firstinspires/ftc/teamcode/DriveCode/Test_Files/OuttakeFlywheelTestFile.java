270b20d8962f53901819d3a13f02f5b53c899ed8
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2025-11-02 14:37:33 -0800

    outtakeFlywheelTestFile can now graph velocity values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/Test_Files/OuttakeFlywheelTestFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/Test_Files/OuttakeFlywheelTestFile.java
@@ -12,6 +12,9 @@ import org.firstinspires.ftc.teamcode.Systems.Outtake;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
 // One of the biggest issue with our flywheels is that they drop in velocity when launching each time. As a result,
 // this file will determine the time it takes for the velocities to return
 
@@ -22,50 +25,40 @@ public class OuttakeFlywheelTestFile extends LinearOpMode {
 
     Outtake outtake;
 
-//    private DcMotorEx rightFlyWheel = null;
-//    private DcMotorEx leftFlyWheel = null;
-
-
-
     @Override
     public void runOpMode() {
-
-//        rightFlyWheel = hardwareMap.get(DcMotorEx.class, "rightFlyWheel");
-//        leftFlyWheel = hardwareMap.get(DcMotorEx.class, "leftFlyWheel");
-//
-//        rightFlyWheel.setDirection(DcMotorEx.Direction.REVERSE);
-//        leftFlyWheel.setDirection(DcMotorEx.Direction.FORWARD);
-//
-//        leftFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        leftFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         gamepad = new GamepadEx(gamepad1);
         outtake = new Outtake(hardwareMap);
 
-        waitForStart();
+        FtcDashboard dashboard = FtcDashboard.getInstance();
 
         List<String> logHistory = new ArrayList<>();
+
+        waitForStart();
 
         //executing
         while (opModeIsActive()) {
             if (gamepad.getButton(GamepadKeys.Button.A)) {
                 outtake.setFlywheelVelocity(2500);
-                //logHistory.clear();
             }
             else if (gamepad.getButton(GamepadKeys.Button.B)) {
                 outtake.setFlywheelVelocity(0);
-                //logHistory.clear();
             }
 
-            logHistory.add("Velocities: " + (int) Math.round(outtake.getCurrentWheelVelocity("left")) + " , " + (int) Math.round(outtake.getCurrentWheelVelocity("right")));
+            //Send data to FTC dashbaord. The dashboard will graph it in real-time
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.put("Left Motor Velocity", outtake.getCurrentWheelVelocity("left"));
+            packet.put("Left Motor Velocity", outtake.getCurrentWheelVelocity("left"));
+            dashboard.sendTelemetryPacket(packet);
 
-            // Display the last few entries (so it doesn’t overflow)
-            int maxLines = 8;
+            //Sends data to the driver station as quantitative values
+            logHistory.add("Velocities: " + (int) Math.round(outtake.getCurrentWheelVelocity("left")) + " , " + (int) Math.round(outtake.getCurrentWheelVelocity("right")));
+            int maxLines = 8; // Display the last few entries (so it doesn’t overflow)
             int start = Math.max(0, logHistory.size() - maxLines);
             for (int i = start; i < logHistory.size(); i++) {
                 telemetry.addLine(logHistory.get(i));
             }
-
             telemetry.update();
             sleep(500);  // prevent spam
         }
