333687e413c9cbafe9053d6613b80489d8946a68
HeyItsMeBen <111411605+HeyItsMeBen@users.noreply.github.com>
2026-01-10 14:26:49 -0800

    changes made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Flywheels.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 public class Flywheels {
 
     private DcMotorEx flywheel = null;
-//    private DcMotorEx leftFlyWheel = null;
+    //    private DcMotorEx leftFlyWheel = null;
 //    private DcMotorEx rightFlyWheel = null;
     final double tickPerRevolution = 28;
 
@@ -31,74 +31,66 @@ public class Flywheels {
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(p, 0, 0, f);
         flywheel.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
+//        leftFlyWheel = hMap.get(DcMotorEx.class, "leftFlyWheel");
+//        rightFlyWheel = hMap.get(DcMotorEx.class, "rightFlyWheel");
+//
+//        leftFlyWheel.setDirection(DcMotorEx.Direction.FORWARD);
+//        rightFlyWheel.setDirection(DcMotorEx.Direction.REVERSE);
+//
+//        leftFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//        leftFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 
-    public void setFlywheelSpeed(double rpm){
+    public void setFlywheelSpeed(double rpm) {
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(p, 0, 0, f);
         flywheel.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
         flywheel.setVelocity(rpm);
     }
 
-    public double launchFromDistance(double distance){ //distance in feet from goal
-        double optimalRpm = 1000+ (83.3 * distance);
+    public double launchFromDistance(double distance) { //distance in feet
+        double optimalRpm = 1000 + (83.3 * distance);
         setFlywheelSpeed(optimalRpm);
         return optimalRpm; //return the optimal rpm for telemetry debugging...
     }
 
-    public void stopFlywheel(){
+    //    public void setFlywheelPower(double power) {
+//        if (power > 1) {
+//            flywheel.setPower(1.0);
+//        } else {
+//            flywheel.setPower(power);
+//        }
+//    }
+    public void stopFlywheel() {
         flywheel.setPower(0);
     }
 
-    public double returnWheelVelocity(){
-        return flywheel.getVelocity();
-    }
-
-    //Update
-    public void setFlywheelVelocity(float givenRPM) {
-        flywheel.setVelocity(tickPerRevolution*(givenRPM/60));
-    }
-
-    public double getFlywheelVelocity(){
-        return flywheel.getVelocity();
-    }
-
-
-
-    @Deprecated
-    public void setFlywheelPower(double power) {
-        if (power > 1) {
-            flywheel.setPower(1.0);
-        } else {
-            flywheel.setPower(power);
-        }
-    }
-
-    @Deprecated
-    public void setRawFlywheelVelocity(float givenRPM){
+    public void setRawFlywheelVelocity(float givenRPM) {
         flywheel.setVelocity(givenRPM);
     }
 
-    public double getRPMFromDistance(double distance){
-        return 1000+ (83.3 * distance);  //return the optimal rpm for telemetry debugging...
-
+    public void setFlywheelVelocity(float givenRPM) {
+        flywheel.setVelocity(tickPerRevolution * (givenRPM / 60));
+//        leftFlyWheel.setVelocity(tickPerRevolution*(givenRPM/60));
+//        rightFlyWheel.setVelocity(tickPerRevolution*(givenRPM/60));
     }
 
-
-    @Deprecated
-    public double getCurrentWheelVelocity(String motor){
+    public double getCurrentWheelVelocity(String motor) {
 //        if (motor.contains("left") || motor.contains("Left")) {
 //            return leftFlyWheel.getVelocity()*60/tickPerRevolution;
 //        } else if (motor.contains("right") || motor.contains("Right")) {
 //            return rightFlyWheel.getVelocity()*60/tickPerRevolution;
 //        }
 
-        return flywheel.getVelocity()*60/tickPerRevolution;
+        return flywheel.getVelocity() * 60 / tickPerRevolution;
 //        return 0;
 
     }
 
-    @Deprecated
-    public double getCurrentWheelRawVelocity(String motor){
+    public double getCurrentWheelRawVelocity(String motor) {
 //        if (motor.contains("left") || motor.contains("Left")) {
 //            return leftFlyWheel.getVelocity();
 //        } else if (motor.contains("right") || motor.contains("Right")) {
@@ -108,15 +100,13 @@ public class Flywheels {
         return flywheel.getVelocity();
     }
 
-    @Deprecated
     public void runOptimalFlywheelVelocity() {
-        flywheel.setVelocity(tickPerRevolution*(optimalVelocity/60));
+        flywheel.setVelocity(tickPerRevolution * (optimalVelocity / 60));
 
 //        leftFlyWheel.setVelocity(tickPerRevolution*(optimalVelocity/60));
 //        rightFlyWheel.setVelocity(tickPerRevolution*(optimalVelocity/60));
     }
 
-    @Deprecated
     public double returnOptimalFlywheelVelocity() {
         return optimalVelocity;
     }
