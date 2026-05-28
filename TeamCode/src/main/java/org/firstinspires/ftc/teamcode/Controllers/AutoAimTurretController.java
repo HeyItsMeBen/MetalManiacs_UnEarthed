e55ae97cd9914f342f9383878763a6974cf20357
FriendOfTheFire <andrewscientist09@gmail.com>
2026-05-28 16:19:02 -0700

    Update AutoAimTurretController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/AutoAimTurretController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/AutoAimTurretController.java
@@ -22,54 +22,58 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AutoAimTurretController {
+    private HardwareMap hMap;
 
-    private boolean cameraAvailable = false;
+    //Mechanisms
     private Turret turret;
     public AutoAim autoAim;
 
-    private VisionPortal visionPortal;
-    private AprilTagProcessor aprilTag;
-
-    private AprilTagDetection desiredTag = null;
-    //private GoBildaPinpointDriver odo;
-
+    //Position-tracking objects
     private MecanumDrive drive;
     private Pose2d initialEstimatedCurrentPose;
     private Vector2d goalPosition;
-    private HardwareMap hMap;
-
     public Pose2d robPos;
 
-    private boolean targetFound = false;
+    //vision objects
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private AprilTagDetection desiredTag = null;
+    //private GoBildaPinpointDriver odo;
+
+    //Vision
+    private int DESIRED_TAG_ID = 24;
     private boolean shouldAutoAim = true;
 
-    private long targetLostStartTime = 0;
+    //target lost or found variables
+    private boolean targetFound = false;
     private boolean wasTargetFoundLastFrame = false;
-
+    private long targetLostStartTime = 0;
     private static final long TARGET_LOST_DELAY_MS = 4000;
 
-    private int DESIRED_TAG_ID = 24;
 
-    boolean localized=false;
-    double lastLocalized =0;
+    public boolean opModeIsActive=true;
+    private static final boolean USE_WEBCAM =true;
+    private boolean cameraAvailable = false;
+    public boolean isRed=true;
 
-    public double turretAngleTelemetry=0;
-
+    //turret
     float turretPower = 0;
-    // Ramp up code
+    public double turretAngleTelemetry=0;
+    double lastTurretAngle=0;
+
+    //ramp-up code + timer
     private float rampUpSpeed = 0.1f; // how fast turret should ramp up to target speed (in seconds)
     double turretStartTime=0;
     double turretStartPower=0;
     double currentTime = 0;
 
-    public boolean opModeIsActive=true;
-    private static final boolean USE_WEBCAM =true;
-    public boolean isRed=true;
-    double lostTagStartTime=0;
-    double lastTurretAngle=0;
+    //unused localization variables
+    boolean localized=false;
+    double lastLocalized =0;
 
     public AutoAimTurretController(HardwareMap hardwareMap, Pose2d givenRobotPosition, String givenTeamColor) {
 
+        //Set team color
         if (givenTeamColor.equals("Blue") || givenTeamColor.equals("blue")){
             isRed=false;
             DESIRED_TAG_ID=20;
@@ -82,9 +86,11 @@ public class AutoAimTurretController {
 
         turret = new Turret(hardwareMap);
         autoAim = new AutoAim(Math.toRadians(0));
+        drive = new MecanumDrive(hardwareMap, givenRobotPosition);
 
         initAprilTag(hardwareMap);
 
+        //unused pinpoint code
 //        odo = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
 //
 //        odo.setOffsets(82.55, 0, DistanceUnit.INCH);
@@ -95,60 +101,18 @@ public class AutoAimTurretController {
 //        Pose2D startingPosition = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0);
 //        odo.setPosition(startingPosition);
         hMap = hardwareMap;
-
-        drive = new MecanumDrive(hMap, givenRobotPosition);
         //drive = new MecanumDrive(hMap, new Pose2d(0,0,Math.toRadians(90)));
 
         initialEstimatedCurrentPose = new Pose2d(drive.localizer.getPose().position.x, drive.localizer.getPose().position.y, drive.localizer.getPose().heading.toDouble()); // x, y, heading in double radians
 
         //April tag stuff
         if (USE_WEBCAM) {
-
             //DO NOT SET GAIN TO ANYTHING HIGHER THAN 255 (it'll go dark)
             setManualExposure(6, 100);  // Use low exposure time to reduce motion blur
         }
     }
 
-    private void initAprilTag(HardwareMap hardwareMap) {
-        try {
-            WebcamName webcam = hardwareMap.tryGet(WebcamName.class, "Webcam 1");
-            if (webcam == null) {
-                cameraAvailable = false;
-                return;
-            }
-
-            aprilTag = new AprilTagProcessor.Builder().build();
-            aprilTag.setDecimation(2);
-
-            visionPortal = new VisionPortal.Builder()
-                    .setCamera(webcam)
-                    .addProcessor(aprilTag)
-                    //.setCameraResolution(new android.util.Size(1280, 720))
-                    .build();
-
-            cameraAvailable = true;
-
-        } catch (Exception e) {
-            cameraAvailable = false;
-        }
-    }
-
-//    public void init() {
-//        aprilTag = new AprilTagProcessor.Builder().build();
-//        aprilTag.setDecimation(2);
-//
-//        VisionPortal.Builder builder = new VisionPortal.Builder()
-//                .addProcessor(aprilTag);
-//
-//        if (useWebcam) {
-//            builder.setCamera(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"));
-//        } else {
-//            builder.setCamera(BuiltinCameraDirection.BACK);
-//        }
-//
-//        visionPortal = builder.build();
-//    }
-
+    //Retrieve information
     public double getCurrentTime(){
         return currentTime;
     }
@@ -170,19 +134,6 @@ public class AutoAimTurretController {
 
         return turretPower;
     }
-
-    public void toggleAutoAim() {
-        shouldAutoAim = !shouldAutoAim;
-    }
-
-    public boolean isAutoAiming() {
-        return shouldAutoAim;
-    }
-
-    public boolean isTargetFound() {
-        return targetFound;
-    }
-
     public double getDistanceToGoalInches() {
         return autoAim.launchPointToGoalCenterX_Distance_Inches;
     }
@@ -190,11 +141,21 @@ public class AutoAimTurretController {
     public double getDistanceToGoalMeters() {
         return autoAim.launchPointToGoalCenterX_Distance_Meters;
     }
+    public boolean isAutoAiming() {
+        return shouldAutoAim;
+    }
+    public boolean isTargetFound() {
+        return targetFound;
+    }
 
+
+    //controls
+    public void toggleAutoAim() {
+        shouldAutoAim = !shouldAutoAim;
+    }
     public void resetTurret() {
         turret.resetInitial();
     }
-
     public void manualControl(boolean left, boolean right) {
 
         if (left) {
@@ -214,19 +175,42 @@ public class AutoAimTurretController {
 //            turret.setMotorPower(0);
         }
     }
+    public void turnToCenter() {
+        turret.rotateTowardsTarget(0);
+    }
 
+    public void setTurretPower(double Power) {
+        turret.setMotorPower(Power);
+    }
+    public void stopTurret() {
+        turretPower = 0;
+        turretStartTime=System.currentTimeMillis();
+        turretStartPower=turret.getTurretPower();
+//        turret.setMotorPower(0);
+    }
+    public void changeColorTo(String teamColor){
+        if (teamColor.equals("Blue") || teamColor.equals("blue")){
+            isRed=false;
+            DESIRED_TAG_ID=20;
+        } else {
+            isRed=true;
+            DESIRED_TAG_ID=24;
+        }
+    }
+
+    //"Update" method variations
     @Deprecated
     public void update(boolean manualLeft, boolean manualRight) {
 
         scanForTags();
 
-        if (!shouldAutoAim) {
+        if (!shouldAutoAim) {   //If we should not auto-aim, then run manual controls and skip everything else
             manualControl(manualLeft, manualRight);
             wasTargetFoundLastFrame = false;
             return;
         }
 
-        if (targetFound) {
+        if (targetFound) {  //Auto-aims using camera when tag is visible
 
             autoAim.calculateEverything(desiredTag);
 
@@ -237,7 +221,7 @@ public class AutoAimTurretController {
 
             wasTargetFoundLastFrame = true;
 
-        } else {
+        } else {    //If tag is not visible...
 
             if (wasTargetFoundLastFrame) {
                 targetLostStartTime = System.currentTimeMillis();
@@ -247,7 +231,7 @@ public class AutoAimTurretController {
             long timeSinceLost =
                     System.currentTimeMillis() - targetLostStartTime;
 
-            if (timeSinceLost >= TARGET_LOST_DELAY_MS) {
+            if (timeSinceLost >= TARGET_LOST_DELAY_MS) { //After waiting for a few seconds, start auto-aiming using pinpoint instead of the camera.
 
                 if (!turret.isAtTargetPosition(0)) {
                     turret.rotateTowardsTarget(0);
@@ -258,7 +242,7 @@ public class AutoAimTurretController {
                     turretStartPower=turret.getTurretPower();
                 }
 
-            } else {
+            } else {    //If we haven't waited long enough, then don't move the turret. Just wait.
                 turretPower = 0;
 //                turret.setMotorPower(0);
                 turretStartTime=System.currentTimeMillis();
@@ -297,7 +281,7 @@ public class AutoAimTurretController {
             turret.runTowardsTargetAngle(autoAim.turretAngle);
         }
     }
-    public void updateWithTimeout(boolean manualLeft, boolean manualRight) {  //untested method that only uses pinpoint to aim if the tag has been lost for over (x) seconds
+    public void updateWithTimeout(boolean manualLeft, boolean manualRight) {  //method that only uses pinpoint to aim if the tag has been lost for over (x) seconds
 
         //turret control
         if (!shouldAutoAim) {
@@ -317,7 +301,7 @@ public class AutoAimTurretController {
             lastTurretAngle=turret.getTurretAngle();
         }
         long timeSinceLost =
-                System.currentTimeMillis() - targetLostStartTime;
+            System.currentTimeMillis() - targetLostStartTime;
         if (timeSinceLost > TARGET_LOST_DELAY_MS) {
             drive.updatePoseEstimate();
             Pose2d RobotPose = drive.localizer.getPose();
@@ -389,7 +373,7 @@ public class AutoAimTurretController {
 //
 //        } else {
 //            turretPower = 0;
-    ////                turret.setMotorPower(0);
+////                turret.setMotorPower(0);
 //            turretStartTime=System.currentTimeMillis();
 //            turretStartPower=turret.getTurretPower();
 //        }
@@ -413,6 +397,7 @@ public class AutoAimTurretController {
         }
     }
 
+    //Vision
     private void scanForTags() {
         if (!cameraAvailable || aprilTag == null) {
             targetFound = false;
@@ -438,6 +423,29 @@ public class AutoAimTurretController {
             cameraAvailable = false;
             targetFound = false;
             desiredTag = null;
+        }
+    }
+    private void initAprilTag(HardwareMap hardwareMap) {
+        try {
+            WebcamName webcam = hardwareMap.tryGet(WebcamName.class, "Webcam 1");
+            if (webcam == null) {
+                cameraAvailable = false;
+                return;
+            }
+
+            aprilTag = new AprilTagProcessor.Builder().build();
+            aprilTag.setDecimation(2);
+
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(webcam)
+                    .addProcessor(aprilTag)
+                    //.setCameraResolution(new android.util.Size(1280, 720))
+                    .build();
+
+            cameraAvailable = true;
+
+        } catch (Exception e) {
+            cameraAvailable = false;
         }
     }
     public boolean isCameraAvailable() {
@@ -474,28 +482,13 @@ public class AutoAimTurretController {
             }
         }
     }
-
-    public void turnToCenter() {
-        turret.rotateTowardsTarget(0);
-    }
-
-    public void stopTurret() {
-        turretPower = 0;
-        turretStartTime=System.currentTimeMillis();
-        turretStartPower=turret.getTurretPower();
-//        turret.setMotorPower(0);
-    }
-
-    public void setTurretPower(double Power) {
-        turret.setMotorPower(Power);
-    }
-
+    //Shutdown
     public void closeWebcam() {
         if (visionPortal != null) {
             visionPortal.close();
         }
     }
-
+    
     public void shutdown() {
         opModeIsActive = false;
         stopTurret();
@@ -505,14 +498,5 @@ public class AutoAimTurretController {
     }
     private double toInches(double meters){
         return meters*39.3700787;
-    }
-    public void changeColorTo(String teamColor){
-        if (teamColor.equals("Blue") || teamColor.equals("blue")){
-            isRed=false;
-            DESIRED_TAG_ID=20;
-        } else {
-            isRed=true;
-            DESIRED_TAG_ID=24;
-        }
     }
 }
