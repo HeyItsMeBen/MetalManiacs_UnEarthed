5e65a79ed899616e67ceaadb48b0098d006b02f5
skullblazed <skullblazed2@gmail.com>
2026-08-29 14:57:43 -0700

    Pushbot V1.6
    smoother turning and deadzone for joysticks + pushing against gravity to hold arm steadily

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/GauranshPushbotCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/GauranshPushbotCode.java
@@ -9,28 +9,32 @@
  *
  *  [HAND / GRIPPER]
  *  RIGHT TRIGGER (hold) = claw outwards
- *  LEFT TRIGGET (hold)  = claw inwards
+ *  LEFT TRIGGER (hold)  = claw inwards
  *
  *  [ARM]
  *  RIGHT STICK Y = arm up/down
  */
-package org.firstinspires.ftc.teamcode.DriveCode;
+
+        package org.firstinspires.ftc.teamcode.DriveCode;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
-import java.util.List;
-
-@TeleOp(name = "PushBot v4a DriveCode-Gauransh", group = "A - TeleOP")
+@TeleOp(name = "PushBot v4b DriveCode-Gauransh", group = "A - TeleOP")
 public class GauranshPushbotCode extends OpMode {
-//variables
+
+    //variables
+
+    public static final double ARM_MANUAL_POWER = 0.7;
+    public static final double ARM_GRAVITY_POWER = 0.1;
+
+    public static final double DEADZONE = 0.25;
+
     public DcMotor leftDrive;
     public DcMotor rightDrive;
     public DcMotor armMotor;
@@ -38,76 +42,106 @@ public class GauranshPushbotCode extends OpMode {
     public Servo rightClaw;
     public GamepadEx driver;
 
+
+
     @Override
     public void init() {
-        leftDrive = hardwareMap.get(DcMotor.class,"leftDrive");
-        rightDrive = hardwareMap.get(DcMotor.class,"rightDrive");
-        armMotor = hardwareMap.get(DcMotor.class,"armMotor");
-        leftClaw = hardwareMap.get(Servo.class,"leftClaw");
-        rightClaw = hardwareMap.get(Servo.class,"rightClaw");
+
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+
         driver = new GamepadEx(gamepad1);
+        // Start with the motors stopped
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        armMotor.setPower(0);
     }
 
     @Override
     public void loop() {
-        //turning left
-        if (driver.getLeftX() < -0.25) {
-            leftDrive.setPower(-1);
-            rightDrive.setPower(1);
+
+        double drive = -driver.getLeftY();
+        double turn = driver.getLeftX();
+
+        // Deadzone
+        if (Math.abs(drive) < DEADZONE) {
+            drive = 0;
         }
-        //turning right
-        if (driver.getLeftX() > 0.25) {
-            leftDrive.setPower(1);
-            rightDrive.setPower(-1);
+
+        if (Math.abs(turn) < DEADZONE) {
+            turn = 0;
         }
-        //going backwards
-        if (driver.getLeftY() < -0.25) {
-            leftDrive.setPower(-1);
-            rightDrive.setPower(-1);
+
+        // drive
+        double leftPower = drive + turn;
+        double rightPower = drive - turn;
+
+        // Keep motor power between -1 and 1
+        leftPower = Range.clip(leftPower, -1, 1);
+        rightPower = Range.clip(rightPower, -1, 1);
+
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+        // arm
+        double armInput = -driver.getRightY();
+
+        if (Math.abs(armInput) > DEADZONE) {
+
+            if (armInput > 0) {
+                armMotor.setPower(ARM_MANUAL_POWER + ARM_GRAVITY_POWER);
+            } else {
+                armMotor.setPower(-ARM_MANUAL_POWER + ARM_GRAVITY_POWER);
+            }
+
+        } else {
+
+            // Hold arm against gravity
+            armMotor.setPower(ARM_GRAVITY_POWER);
         }
-        //going forwards
-        if (driver.getLeftY() > 0.25) {
-            leftDrive.setPower(1);
-            rightDrive.setPower(1);
-        }
-        //move arm up
-        if (driver.getRightY() > 0.25) {
-            armMotor.setPower(1);
-        }
-        //move arm down
-        if (driver.getRightY() < -0.25) {
-            armMotor.setPower(-1);
-        }
-        //claw movement inwards
-        if (driver.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.2) {
+
+        double leftTrigger =
+                driver.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER);
+
+        double rightTrigger =
+                driver.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+
+        // Left trigger = claw inward
+        if (leftTrigger > 0.2) {
+
             leftClaw.setPosition(0.2);
             rightClaw.setPosition(0.2);
         }
-        //claw movement outwards
-        if (driver.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.2) {
+
+        // Right trigger = claw outward
+        else if (rightTrigger > 0.2) {
+
             leftClaw.setPosition(0);
             rightClaw.setPosition(0);
         }
-        //stopping the movements
-        if (driver.getLeftY() == 0) {
-            leftDrive.setPower(0);
-            rightDrive.setPower(0);
-        }
-        if (driver.getLeftX() == 0) {
-            leftDrive.setPower(0);
-            rightDrive.setPower(0);
-        }
-        if (driver.getRightY() == 0) {
-            armMotor.setPower(0);
-        }
+
+        telemetry.addData("Left Drive", leftPower);
+        telemetry.addData("Right Drive", rightPower);
+        telemetry.addData("Arm", armMotor.getPower());
+        telemetry.addData("Left Claw", leftTrigger);
+        telemetry.addData("Right Claw", rightTrigger);
+        telemetry.update();
     }
 
     @Override
     public void stop() {
+
         armMotor.setPower(0);
-        leftClaw.setPosition(0);
-        rightClaw.setPosition(0);
+
         leftDrive.setPower(0);
         rightDrive.setPower(0);
+
+        leftClaw.setPosition(0);
+        rightClaw.setPosition(0);
     }
 }
