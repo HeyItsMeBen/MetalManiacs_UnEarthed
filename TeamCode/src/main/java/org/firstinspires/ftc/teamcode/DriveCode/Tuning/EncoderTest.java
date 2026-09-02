6eb3b6aa08a94175c4aac5264cdb91977c2c6183
Bernie Codez <166939245+BernieCodez@users.noreply.github.com>
2026-09-01 17:31:46 -0700

    Pushbot v2.1.1 Encoder Tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode/Tuning/EncoderTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.DriveCode.Tuning;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "Encoder Test")
+public class EncoderTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "armMotor");
+
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            telemetry.addData("Encoder Ticks", motor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
